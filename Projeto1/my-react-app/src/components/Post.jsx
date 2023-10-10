3325cdbe37d643eb12c8a295a2a5a851caab5dda
JackelineMatos <jackeline.matos@gmail.com>
2023-10-10 18:31:33 +0200

    componente-Commentar-emAndamento

--- Projeto1/my-react-app/src/components/Post.jsx
+++ Projeto1/my-react-app/src/components/Post.jsx
@@ -14,11 +14,13 @@
 
 //Conceito de de Estado       
 
-
+import { format, formatDistanceToNow } from "date-fns"
+import { } from "date-fns/locale/pt-BR";
         
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"       
+import { ptBR } from "date-fns/locale";
 
 
 //Conceito de de Estado
@@ -29,12 +31,32 @@ const comments = [
     3,
 ]
 
+
+
 //As propriedades podem ser exportadas da seguinte forma
 //export function Post(props){
 //mas também podemos fazer a desestruturacao com abaixo
-export function Post({author}){
+export function Post({author, publishedAt, content}){
 
+ /* 
+
+    Opcao de data utiliyada pelo JS ; Intl.DateTimeFormat
  
+    const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
+    day: "2-digit",
+    month: "long",
+    houer: "2-digit",
+    minute: "2-digit",
+ }).format(publishedAt) */
+
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+        locale:ptBR,
+    } );
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale:ptBR,
+        addSuffix: true,
+    })
     return <>
         <article className={styles.post}>
             <header>
@@ -49,11 +71,20 @@ export function Post({author}){
                 {/* tag que se refere a data e tempo <time></time>e
                     permite receber o atributo dateTime=""
                  */}
-                <time title="11 de maio publicado às 11:13" dateTime="2023-09-15">Publicado há 1 hora</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-
+                {content.map(line => {
+                    if(line.type === "paragraph"){
+                        return <p>{line.content}</p>;
+                    }else if (line.type === "link"){
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
+                 
               
             </div>
 
